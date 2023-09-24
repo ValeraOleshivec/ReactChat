07bc58455582d0ec116686949998294302ac7ea6
ValeraOleshivec <70708412+ValeraOleshivec@users.noreply.github.com>
2023-09-24 14:22:39 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 const http = require("http").Server(app);
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "https://react-chat-three-phi.vercel.app/",
+    origin: "https://reliable-yeot-bdb44f.netlify.app",
   },
 });
 
